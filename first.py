5eafdf7ca5014f282a26825377e323187baa9a63
dilen1999 <dilengeorge@gmail.com>
2026-05-29 19:30:20 +0530

    feat; string concatenation

---
+++ first.py
@@ -0,0 +1,8 @@
+print("First Program - Python print Function")
+print("It is declaned like This:")
+print("print('Hello, World!')")
+
+print('test01')
+
+print("hello" + " " + "Dilen")
+print("hello "+ "Dilen")
