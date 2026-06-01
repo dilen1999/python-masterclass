e731e29ea0b4d14c2838b45f6b19b303ec0699eb
dilen1999 <dilengeorge@gmail.com>
2026-06-01 21:37:17 +0530

    feat: bodmas

---
+++ Ex2.py
@@ -0,0 +1,7 @@
+number = input("Enter a Two digit number: ")
+first_digit = number[0]
+second_digit = number[1]
+sum_of_digits = int(first_digit) + int(second_digit)
+print("The sum of the digits is:", sum_of_digits)
+
+print (2**3+ 6/3 - 5*2)
