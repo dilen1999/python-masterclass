93b41f307ce8144d9e6bd3115590d4f4a480115c
dilen1999 <dilengeorge@gmail.com>
2026-06-02 21:21:22 +0530

    feat:  round

---
+++ round.py
@@ -0,0 +1,13 @@
+print(round(5.615361))
+print(round(5.615361, 0))
+print(round(5.615361, 1))
+print(round(5.615361, 2))
+print(round(5.615361, 3))
+print(round(5.615361, 4))
+print(round(5.615361, 5))
+
+print(round(6.74, -1))
+print(round(6.74, -2))
+
+print(round(665, -1))
+print(round(675, -1)) 
