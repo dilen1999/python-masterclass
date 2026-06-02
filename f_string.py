81435b6fa83904cb54911b3693e800b58972979c
dilen1999 <dilengeorge@gmail.com>
2026-06-02 21:40:46 +0530

    feat: f string

---
+++ f_string.py
@@ -0,0 +1,6 @@
+name = "George Dilen"
+age = 25
+height = 1.75
+
+print(f"My name is {name}, I am {age} years old and I am {height} meters tall.")
+print(f"My father's name is {name}, he is {age*2} years old and he is {height} meters tall.")
