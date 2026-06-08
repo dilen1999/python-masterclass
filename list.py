e6a4f3e36dc70fc42779627c8d4c24935f716c4f
dilen1999 <dilengeorge@gmail.com>
2026-06-08 21:30:16 +0530

    feat: list

---
+++ list.py
@@ -0,0 +1,12 @@
+rollNo = [1, 2, 3, 4, 5]
+print(rollNo[0])
+print(rollNo[1])
+
+names = ["Alice", "Bob", "Charlie", "David", "Eve"]
+print(names[0])
+print(names[1])
+
+
+mixedList = [1, "Alice", 3.14, True]
+print(mixedList[0])
+print(mixedList[1])
