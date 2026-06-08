aa3795d2232b9c464d445ddd9e7d6e16cdb00d69
dilen1999 <dilengeorge@gmail.com>
2026-06-08 21:36:42 +0530

    feat: list add

--- list.py
+++ list.py
@@ -1,6 +1,15 @@
 rollNo = [1, 2, 3, 4, 5]
 print(rollNo[0])
 print(rollNo[1])
+rollNo.reverse()
+rollNo.append(6)
+rollNo.insert(2, 10)
+rollNo.extend([7, 8, 9])
+rollNo.remove(10)
+
+print(rollNo)
+
+
 
 names = ["Alice", "Bob", "Charlie", "David", "Eve"]
 print(names[0])
