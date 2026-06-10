7d0f8fb714d43fd276db0c41ead530a448e894c4
dilen1999 <dilengeorge@gmail.com>
2026-06-10 21:37:31 +0530

    feat: for loop

---
+++ forloop.py
@@ -0,0 +1,13 @@
+names = ["John", "Jane", "Doe", "Smith"]
+
+for name in names:
+    print(name)
+
+
+numbers = [1, 2, 3, 4, 5]
+for number in numbers:
+    print(number * 2)
+
+fruits = 'Apple'
+for fruit in fruits:
+    print(fruit)
